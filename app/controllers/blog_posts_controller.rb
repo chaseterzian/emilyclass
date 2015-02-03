a296daba1f254d3df07d37201856705884431f7c
Andrew Deitrick <ajdeitrick@gmail.com>
2015-02-03 16:17:42 -0700

    post request, creates object in database

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -6,4 +6,17 @@ class BlogPostsController < ApplicationController
   def new
     @blog_post = BlogPost.new
   end
+
+  def create
+    blog_post = BlogPost.new(blog_post_params)
+    blog_post.save
+
+    redirect_to blog_posts_path
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :author, :content)
+  end
 end
